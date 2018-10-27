50b9099cce6561e70dfd25253ca94ca898ddaafb
devehoper <devehoper@gmail.com>
2018-10-27 22:39:44 +0100

    Added fontawesome

--- Core/Config.js
+++ Core/Config.js
@@ -1,3 +1,15 @@
 var Config = {
-    base_url: "file:///home/devehoper/Desktop/repositorios/feigniter/index.html"
+    base_url: "file:///home/devehoper/Desktop/repositorios/feigniter/index.html",
+    controllers_path: "Controller/",
+    models_path: "Model/",
+    views_path: "View/",
+    default_controller: "Home",
+
+    /**
+     * Defining App routing system
+     */
+    routes: {
+        //Controller:"Method"
+        Home: "index()"
+    }
 }
